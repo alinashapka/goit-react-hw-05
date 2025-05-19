e23861d660093f5d062b59cbde6b753dc6fa8745
alina <alinkash2811@gmail.com>
2025-05-19 10:30:03 +0200

    add vercel.json

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,15 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter } from "react-router-dom";
 import './index.css'
 import App from './components/App.jsx';
 import "modern-normalize";
 
 createRoot(document.getElementById('root')).render(
+
+  <BrowserRouter>
   <StrictMode>
     <App />
-  </StrictMode>,
+    </StrictMode>
+    </BrowserRouter>,
 )
